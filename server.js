1a945dc7e5f3770e8112e1cb65edbe434a158e66
Joaquim707 <158746033+Joaquim707@users.noreply.github.com>
2025-05-08 16:19:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+// require("./config/db");
+
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 10000;
+
+// const UserRouter = require("./api/User");
+
+// app.use(express.json()); // for parsing JSON request bodies
+
+// app.use("/user", UserRouter); // fixed route prefix
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
 require("./config/db");
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 const UserRouter = require("./api/User");
 
-app.use(express.json()); // for parsing JSON request bodies
+app.use(express.json());
 
-app.use("/user", UserRouter); // fixed route prefix
+app.get("/", (req, res) => {
+  res.send("API is live 🚀");
+});
+
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
